ab3cd1af39050094e3b38ec59061e055f91dfa37
Shapchits Anastasiya <shapchits.ad@phystech.edu>
2021-10-09 22:11:43 +0300

    Выделила и описала функцию draw_picture.

--- picture.py
+++ picture.py
@@ -1,10 +1,25 @@
 import pygame as pg
+
+
+def draw_picture(screen, x, y, width, height):
+    """
+    Рисует картинку
+
+    : param screen: дисплей pygame, на котором будет изображение
+    : param x: левая координата прямоугольника с рисунком
+    : param y: верхняя координата прямоугольника с рисунком
+    : param width: ширина прямоугольника
+    : param height: высота прямоугольника
+    """
+    print('Рисую картинку.', x, y, width, height)
+
+
 pg.init()
 width, height = screen_size = (300, 200)
 screen = pg.display.set_mode(screen_size)
 
 # Здесь будем рисовать
-
+draw_picture(screen, 0, 0, width, height)
 pg.display.update()
 
 work_flag = True
